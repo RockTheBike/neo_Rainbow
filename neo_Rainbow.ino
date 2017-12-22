43d1343952699ce233efe821f1092186c2a70f1d
shop <shop@Rock-the-Bike.local>
2017-12-21 18:25:30 -0800

    this is just how it is now

--- neo_Rainbow.ino
+++ neo_Rainbow.ino
@@ -1,8 +1,9 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 7
+#define PIN 12
+#define LEDCount 420
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(129, PIN, NEO_GRB + NEO_KHZ800); //first number controls the amount of pixels you have (add 4 so the drip falls off the edge)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDCount, PIN, NEO_GRB + NEO_KHZ800); //first number controls the amount of pixels you have (add 4 so the drip falls off the edge)
 
 void setup() {
   strip.begin();
@@ -10,16 +11,14 @@ void setup() {
 }
 void loop() {
   int p=1000;
-  colorWipe(255, 0, 0, p); // Red
-  colorWipe(0, 255, 0, p); // Green
-  colorWipe(0, 0, 255, p); // Blue
+  colorWipe(25, 0, 0, p); // Red
+  colorWipe(0, 25, 0, p); // Green
+  colorWipe(0, 0, 25, p); // Blue
 }
 void colorWipe(int r, int g, int b, int wait) {
   for(int i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, strip.Color(r/10,g/10,b/10));
+      strip.setPixelColor(i, strip.Color(r,g,b));
   }
   strip.show();
   delay(wait);  
 }
-
-
