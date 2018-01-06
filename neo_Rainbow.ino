e4474a37436f5b2012dab040b33bf4265bb81332
shop <shop@Rock-the-Bike.local>
2018-01-05 16:28:08 -0800

    light up 720 LEDs on tubular pedalometers

--- neo_Rainbow.ino
+++ neo_Rainbow.ino
@@ -1,19 +1,20 @@
 #include <Adafruit_NeoPixel.h>
 
 #define PIN 12
-#define LEDCount 20
+#define LEDCount 120
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDCount, PIN, NEO_GRB + NEO_KHZ800,3); //first number controls the amount of pixels you have (add 4 so the drip falls off the edge)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDCount, PIN, NEO_GRB + NEO_KHZ800,6); //first number controls the amount of pixels you have (add 4 so the drip falls off the edge)
 
 void setup() {
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
+  strip.setBrightness(128); // set overall brightness to half
 }
 void loop() {
   int p=1000;
-  colorWipe(25, 0, 0, p); // Red
-  colorWipe(0, 25, 0, p); // Green
-  colorWipe(0, 0, 25, p); // Blue
+  colorWipe(255, 0, 0, p); // Red
+  colorWipe(0, 255, 0, p); // Green
+  colorWipe(0, 0, 255, p); // Blue
 }
 void colorWipe(int r, int g, int b, int wait) {
   for(int i=0; i<strip.numPixels(); i++) {
