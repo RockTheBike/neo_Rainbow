db5f5748f96b240fbd3309e329864862b1c233dc
jake waters <jerkey@g-cipher.net>
2018-01-05 16:33:10 -0800

    cycle colors RGBW 3x per second

--- neo_Rainbow.ino
+++ neo_Rainbow.ino
@@ -1,20 +1,21 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 12
-#define LEDCount 120
+#define PIN 13
+#define LEDCount 48
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDCount, PIN, NEO_GRB + NEO_KHZ800,6); //first number controls the amount of pixels you have (add 4 so the drip falls off the edge)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDCount, PIN, NEO_GRB + NEO_KHZ800); //first number controls the amount of pixels you have (add 4 so the drip falls off the edge)
 
 void setup() {
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
-  strip.setBrightness(128); // set overall brightness to half
+  strip.setBrightness(32);
 }
 void loop() {
-  int p=1000;
+  int p=300;
   colorWipe(255, 0, 0, p); // Red
   colorWipe(0, 255, 0, p); // Green
   colorWipe(0, 0, 255, p); // Blue
+  colorWipe(255, 255, 255, p); // white
 }
 void colorWipe(int r, int g, int b, int wait) {
   for(int i=0; i<strip.numPixels(); i++) {
